3384e1397a0a2ffb9bfe6c69a25fb9735f9f6d14
GabrielSWright <35936487+GabrielSWright@users.noreply.github.com>
2018-04-29 18:31:53 -0400

    Crawls Tweets without job stuff

--- first/first/crawler.py
+++ first/first/crawler.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import os, string
 import time, tweepy, json
 
@@ -13,27 +12,30 @@ api = tweepy.API(auth)
 
 
 class FilteredStream(tweepy.StreamListener):
-    def __init__(self,time_limit=60):
-        self.start_time = time.time()
-        self.limit = time_limit
+    def __init__(self):
+        self.counter = 0
+        self.limit = 100
         os.system("touch tmp_results")
         self.File = open("tmp_results.txt","r+")
         super(FilteredStream,self).__init__()
     
     def on_status(self,status):
         try:
-                if (time.time() - self.start_time) < self.limit:
+            text = status.text.replace(',','^^^').replace('\n',"").replace("-&gt","")
+            text = text.split()
+            if "#Job" not in text and "#job" not in text and "#Job:" not in text and "#job:" not in text and "#Job?" not in text and "#hiring" not in text and "#CareerArc" not in text:
                     if type(status.geo)!=type(None):
                         self.File.write("{0},{1},{2},{3},{4}\n".format(status.id_str,status.text.replace(',','^^^').replace('\n',"").replace('-&gt',""),status.geo['coordinates'][0],status.geo['coordinates'][1],status.created_at))
-                    return True
-                else:
-                    self.File.close()
-                    return False
+                        self.counter += 1
+                        print(self.counter)
+            if self.counter==self.limit:
+                self.File.close()
+                return False
         except UnicodeEncodeError:
-                pass
+            pass
 
 
 os.system("touch tmp_results.txt")
-stream = tweepy.Stream(auth=api.auth,listener=FilteredStream(time_limit=10))
+stream = tweepy.Stream(auth=api.auth,listener=FilteredStream())
 stream.filter(locations=[-124,24,-66,49])
 print("done")
